85fd0a2d2e8b5690f9cfc342b1eaeacac28a8d78
mohammedarqam <mohammedarqam97@gmail.com>
2018-09-18 00:50:07 +0530

    Routing and Libs installed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signUp', component: SignUpComponent },
+  { path: 'dashboard', component: DashboardComponent  },
+  {path: '404', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/404'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,46 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import * as firebase from 'firebase';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+
+
+
+
+export const firebaseCred = {
+  apiKey: "AIzaSyCtgOPbmMn2PhB307Jztz0wo8Zku1ToRME",
+  authDomain: "easylogin-e5fa3.firebaseapp.com",
+  databaseURL: "https://easylogin-e5fa3.firebaseio.com",
+  projectId: "easylogin-e5fa3",
+  storageBucket: "easylogin-e5fa3.appspot.com",
+  messagingSenderId: "852715724645"
+};
+firebase.initializeApp(firebaseCred);
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    DashboardComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    HomeComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseCred),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
